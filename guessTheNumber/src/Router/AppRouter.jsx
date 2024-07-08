85d28af0e0a31ea747fec3dd163ec25538741938
Sonal Sai K S <sonal.sai@aot-technologies.com>
2024-07-08 09:51:29 -0700

    BAse updated

--- guessTheNumber/src/Router/AppRouter.jsx
+++ guessTheNumber/src/Router/AppRouter.jsx
@@ -3,7 +3,7 @@ import Index from "../pages/Index";
 
 const AppRouter = () => {
     return (
-        <Router>
+        <Router basename="/Guees_The_Number">
             <Routes>
                 <Route path="/" element={<Index/>} />
             </Routes>
